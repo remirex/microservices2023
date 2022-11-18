2e0eb4edf6358158ccf1dd45c8d4ba166aee2676
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2022-11-18 18:31:15 +0100

    initializing router

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
+import {exampleRouter} from "./routes/example";
+
 const app = express();
 
-app.get('/api/users/test', (req, res) => {
-    res.send('Hi there!');
-});
+// load auth routes
+app.use('/api/users',exampleRouter);
 
 app.listen(3002, () => {
     console.log('Listening on port 3002222.')
