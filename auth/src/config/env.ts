c78b6569f2dd03af808bf20dbfdfcccee7561ecf
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2022-11-21 16:38:31 +0100

    env init

---
+++ auth/src/config/env.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import Joi from 'joi';
+
+dotenv.config({path: path.join(__dirname, '../../.env')});
+
+const envVarSchema = Joi.object().keys({
+    NODE_ENV: Joi.string().valid('production', 'development', 'staging', 'test', 'local').required(),
+    PORT: Joi.number().default(3000),
+}).unknown();
+
+const { value: envVars, error } = envVarSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+export default {
+    env: envVars.NODE_ENV,
+    port: envVars.PORT,
+}
+
